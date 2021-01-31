4f320d659eabd4bf98a0bb35f440c80b502cd71b
ahmadallouch <50431754+ahmadallouch@users.noreply.github.com>
2021-01-31 14:57:47 -0500

    Temporary Weekly File

---
+++ temp.py
@@ -0,0 +1,16 @@
+
+count = 2
+while count == 0:
+    print(count)
+
+else:
+    print("Count does not equal to 0")
+    count = 0
+
+while count < 5:
+    print(count)
+    count += 1
+
+print(count)
+
+print(count + 2)

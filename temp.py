aa7aff9a4e14f738795e22a6e0e28097a5c35879
ahmadallouch <50431754+ahmadallouch@users.noreply.github.com>
2021-02-09 23:18:22 -0500

    Update temp.py

--- temp.py
+++ temp.py
@@ -1,16 +1,1 @@
-
-count = 2
-while count == 0:
-    print(count)
-
-else:
-    print("Count does not equal to 0")
-    count = 0
-
-while count < 5:
-    print(count)
-    count += 1
-
-print(count)
-
-print(count + 2)
+print("Hello World")
